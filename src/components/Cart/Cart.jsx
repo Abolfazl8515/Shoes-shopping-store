8e9ab83c2f361f6de91f8907f070756e19b724d7
Abolfazl <boorbooryabolfazl85@gmail.com>
2023-05-08 18:54:28 +0330

    added cart responseive

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,29 +1,31 @@
 const Cart = () => {
   return (
-    <div className="w-3/4 mx-auto flex flex-col">
+    <div className="md:w-3/4 w-5/6 mx-auto flex flex-col">
       <div className="text-left">
         <h2 className="text-4xl font-bold">Your Cart</h2>
       </div>
-      <div className="w-full flex justify-between">
-        <div className="my-5 flex">
-          <div className="flex items-center">
-            <img
-              src="src/assets/Images/Shoes.png"
-              alt="Products image"
-              width="120"
-              height="120"
-              className="rounded-xl"
-            />
-            <h3 className="ml-4 font-bold text-lg">Adidas Shoes</h3>
-          </div>
-          <div className="flex items-center mx-5">
-            <p className="mr-5">$113</p>
-            <button type="button" className="text-red-400">
-              Delete
-            </button>
+      <div className="w-full md:flex md:justify-between">
+        <div className="flex flex-col md:w-2/3 w-full">
+          <div className="my-5 flex justify-between flex-wrap w-full border-b border-b-cyan-700 border-solid">
+            <div className="flex items-center">
+              <img
+                src="src/assets/Images/Shoes.png"
+                alt="Products image"
+                width="120"
+                height="120"
+                className="rounded-xl"
+              />
+              <h3 className="ml-4 font-bold text-lg">Adidas Shoes</h3>
+            </div>
+            <div className="flex items-center mx-5">
+              <p className="mr-5">$113</p>
+              <button type="button" className="text-red-400">
+                Delete
+              </button>
+            </div>
           </div>
         </div>
-        <div>
+        <div className="text-left md:w-1/4 w-full">
           <p>Total: $113</p>
           <p>Off price : - $12</p>
           <p>Your pay price : $101</p>
