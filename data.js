bd00e44328fc20cb60537dd62b33db8fb4529537
Abolfazl <boorbooryabolfazl85@gmail.com>
2023-05-11 18:33:46 +0330

    added data file

---
+++ data.js
@@ -0,0 +1,10 @@
+export const data = [
+  { id: 1, title: "Adidas", price: 113, src: "src/assets/Images/adidas.jfif" },
+  { id: 2, title: "Alstar", price: 241, src: "src/assets/Images/alstar.jfif" },
+  { id: 3, title: "Asics", price: 157, src: "src/assets/Images/asics.jfif" },
+  { id: 4, title: "Fila", price: 118, src: "src/assets/Images/fila.jfif" },
+  { id: 5, title: "NB", price: 113, src: "src/assets/Images/NB.jfif" },
+  { id: 6, title: "Nike", price: 358, src: "src/assets/Images/nike.jfif" },
+  { id: 7, title: "Puma", price: 120, src: "src/assets/Images/puma.jfif" },
+  { id: 8, title: "Jordan", price: 200, src: "src/assets/Images/jordan.jfif" },
+];
